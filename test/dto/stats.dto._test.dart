cc0771db2f3e670a6c31000b07b663322dd85806
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-11-29 19:24:28 -0300

    feat: first test finish

--- test/dto/stats.dto._test.dart
+++ test/dto/stats.dto._test.dart
@@ -2,10 +2,12 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pokedex/dto/stats.dto.dart';
+import 'package:path/path.dart';
 
-void main(List<String> args) {
+void main() {
+  final currDir = dirname(Platform.script.path);
   test("test fromJson StatDto", () async {
-    final fileJson = File("./stat.json");
+    final fileJson = File('test/dto/stat.json');
     final stat = StatsDto.fromJson(json.decode(await fileJson.readAsString()));
     expect(stat.name, "hp");
     expect(stat.point, 45);
